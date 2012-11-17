d56272a95e8078652c6bfec189b72ed236ec20da
GABRIEL GASTER <gaber@ubuntu.(none)>
2012-11-16 20:15:11 -0600

    added_min_sparse

---
+++ min_sparse.py
@@ -0,0 +1,22 @@
+import numpy as np
+from scipy import sparse
+
+def min_rows_sparse(X):
+    if type(X) is not sparse.csc_matrix:
+        X = X.tocsc()
+    return np.array(map(min_sparse,map(X.getcol,xrange(X.shape[1]))))
+
+def min_cols_sparse(X):
+    if type(X) is not sparse.csc_matrix:
+        X = X.tocsc()
+    return np.array(map(min_sparse,map(X.getrow,xrange(X.shape[0]))))
+
+def min_sparse(X):
+    if type(X) is not sparse.csc_matrix:
+        X = X.tocsc()
+    m = X.data
+    if len(m) == 0:
+        return 0
+    else:
+        m = m.min()
+    return m if X.getnnz() == X.size else min(m, 0)
